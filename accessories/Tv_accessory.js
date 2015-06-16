dab9ba3e7c954ca64d04403bbc44325d2d2701a4
Matthijs Otterloo <matthijsotterloo@users.noreply.github.com>
2015-06-16 20:21:14 +0200

    Added new accessories file

---
+++ accessories/Tv_accessory.js
@@ -0,0 +1,93 @@
+// HomeKit types required
+var types = require("./types.js")
+var exports = module.exports = {};
+
+var execute = function(accessory,characteristic,value){ console.log("executed accessory: " + accessory + ", and characteristic: " + characteristic + ", with value: " +  value + "."); }
+
+var SamsungRemote = require('samsung-remote');
+var remote = new SamsungRemote({
+    ip: '192.168.178.113' // required: IP address of your Samsung Smart TV (replace this)
+});
+
+exports.accessory = {
+  displayName: "Samsung Smart TV",
+  username: "1A:2B:3C:4D:5E:FF",
+  pincode: "031-45-154",
+  services: [{
+    sType: types.ACCESSORY_INFORMATION_STYPE, 
+    characteristics: [{
+    	cType: types.NAME_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Samsung Smart TV",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.MANUFACTURER_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Oltica",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.MODEL_CTYPE,
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Rev-1",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.SERIAL_NUMBER_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "A1S2NASF88EW",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255    
+    },{
+    	cType: types.IDENTIFY_CTYPE, 
+    	onUpdate: null,
+    	perms: ["pw"],
+		format: "bool",
+		initialValue: false,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Identify Accessory",
+		designedMaxLength: 1    
+    }]
+  },{
+    sType: types.LIGHTBULB_STYPE, 
+    characteristics: [{
+    	cType: types.NAME_CTYPE,
+    	onUpdate: null,
+    	perms: ["pr"],
+		format: "string",
+		initialValue: "Light 1 Light Service",
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Bla",
+		designedMaxLength: 255   
+    },{
+    	cType: types.POWER_STATE_CTYPE,
+    	onUpdate: function(value) { console.log("Change:",value); execute("KEY_VOLUP", "Volume UP", value); }, // This send the volume up command you can replace this
+    	perms: ["pw","pr","ev"],
+		format: "bool",
+		initialValue: false,
+		supportEvents: false,
+		supportBonjour: false,
+		manfDescription: "Turn On the Light",
+		designedMaxLength: 1    
+    }]
+  }]
+}
